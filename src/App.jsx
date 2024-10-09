e93bd46c4a5a31fff7d97e02b3b1bf549454184c
BrenoLemesD <brenolemesd@gmail.com>
2024-10-09 09:28:46 -0300

    Estruturando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,84 @@
-import { useState } from "react";
+// CSS
 import "./App.css";
+// React
+import { useCallback, useEffect, useState } from "react";
+// Data
+import { wordsList } from "./data/words";
+// Components
+import StartScreen from "./components/StartScreen";
+import GameOver from "./components/GameOver";
+import Game from "./components/Game";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [latters, setLatters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);
+
+    //pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(word);
+
+    return [word, category];
+  };
+
+  //Starts the secret Word Game
+  const startGame = () => {
+    // pick Word and pick category
+    const { word, category } = pickWordAndCategory();
+
+    //create an array of latters
+    let WordLetters = word.split("");
+
+    WordLetters = WordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(WordLetters);
+
+    //fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLatters(letters);
+
+    setGameStage(stages[1].name);
+  };
+
+  // process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  //restart the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
 
   return (
     <>
-      <div>HEHE TA OLHANDO OQUE??</div>
+      <div className="App">
+        {/* Só executa o StartScree se o game stage for 'start!' */}
+        {gameStage === "start" && <StartScreen startGame={startGame} />}
+        {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+        {gameStage === "end" && <GameOver retry={retry} />}
+      </div>
     </>
   );
 }
